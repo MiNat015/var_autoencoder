8f826b054b5e5fd227497a63d656318d5285337f
Arjun Srikanth <arjunsrikanth@Arjuns-MacBook-Air.local>
2024-07-04 14:11:30 +0530

    Added VAE class

---
+++ modules.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+import numpy as np
+
+class VAE(nn.Module):
+    """
+    Args:
+        nn (_type_): _description_
+    """
+    def __init__(self, input_dim, hidden_dim=400, latent_dim=200, device=torch.device("cpu")):
+        super(VAE, self).__init__()
+        self.device = device
+
+        self.encoder = nn.Sequential(
+        nn.Linear(input_dim, hidden_dim),
+        nn.LeakyReLU(0.2),
+        nn.Linear(hidden_dim, latent_dim),
+        nn.LeakyReLU(0.2),
+        )
+
+        # Latent mean and variance
+        self.mean_layer = nn.Linear(latent_dim, 2)
+        self.logvar_layer = nn.Linear(latent_dim, 2)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(2, latent_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(latent_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+        )
+
+    def encode(self, x):
+        x = self.encoder(x)
+        mean = self.mean_layer(x)
+        logvar = self.logvar_layer(x)
+        return mean, logvar
+
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var).to(self.device)      
+        z = mean + var*epsilon
+        return z
+    
+    def decode(self, x):
+        return self.decoder(x)
+    
+    def forward(self, x):
+        mean, logvar = self.encode(x)
+        z = self.reparameterization(mean, logvar)
+        x_hat = self.decoder(z)
+        return x_hat, mean, logvar
