b1f07c7af1591a98f7bbd9da3b3f7dacd582d07a
Arjun Srikanth <arjunsrikanth@Arjuns-MacBook-Air.local>
2024-07-23 17:27:36 +1000

    Added a function to visualise images in dataset

--- modules.py
+++ modules.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 import numpy as np
 
+from mpl_toolkits.axes_grid1 import ImageGrid
+
 class VAE(nn.Module):
     """
     Args:
@@ -56,3 +58,26 @@ def loss_function(x, x_hat, mean, log_var):
     reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
     KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
     return reproduction_loss + KLD
+
+def visualise_dataset(train_loader, plt, grayscale=True):
+    dataiter = iter(train_loader)
+    image = next(dataiter)
+
+    num_samples = 25
+
+    if grayscale:
+        sample_images = [image[0][i,0] for i in range(num_samples)]
+    else:
+        sample_images = [image[0][i].permute(1, 2, 0) for i in range(num_samples)]
+
+    fig = plt.figure(figsize=(5, 5))
+    grid = ImageGrid(fig, 111, nrows_ncols=(5, 5), axes_pad=0.1)
+
+    for ax, im in zip(grid, sample_images):
+        if grayscale:
+            ax.imshow(im, cmap='gray')
+        else:
+            ax.imshow(im.numpy())
+        ax.axis('off')
+
+    plt.show()
